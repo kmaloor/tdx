d7fec84caa8a708ecd338f22c0f608c851e9582a
Wei Wang <wei.w.wang@intel.com>
2023-01-17 10:30:03 +0800

    x86/tdx: retry tdcalls on TDX_OPERAND_BUSY related errors
    TDX_OPERAND_BUSY and TDX_OPERAND_BUSY_HOST_PRIORITY errors are returned
    to the guest when its tdcall operations fail to acquire some TDX internal
    resources (e.g. locks), and the guest is expected to retry the operations.
    
    Limit the number of retires to TDX_MODULECALL_RETRY_MAX, and exceeding
    this number is deemed as an issue and need to fail out.
    
    Signed-off-by: Wei Wang <wei.w.wang@intel.com>

--- arch/x86/include/asm/shared/tdx.h
+++ arch/x86/include/asm/shared/tdx.h
@@ -26,6 +26,12 @@
 #define TDVMCALL_SETUP_NOTIFY_INTR	0x10004
 #define TDVMCALL_GET_QUOTE		0x10002
 
+#define TDX_MODULECALL_RETRY_MAX	10000
+#define TDX_MODULECALL_STATUS_MASK	0xFFFFFFFF00000000ULL
+
+#define TDX_OPERAND_BUSY		0x8000020000000000ULL
+#define TDX_OPERAND_BUSY_HOST_PRIORITY	0x8000020400000000ULL
+
 #ifndef __ASSEMBLY__
 
 /*
@@ -64,7 +70,6 @@ static inline u64 _tdx_hypercall(u64 fn, u64 r12, u64 r13, u64 r14, u64 r15)
 	return __tdx_hypercall(&args, 0);
 }
 
-
 /* Called from __tdx_hypercall() for unrecoverable failure */
 void __tdx_hypercall_failed(void);
 
@@ -85,9 +90,28 @@ struct tdx_module_output {
 };
 
 /* Used to communicate with the TDX module */
-u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
-		      u64 r10, u64 r11, u64 r12, u64 r13,
-		      struct tdx_module_output *out);
+u64 __tdx_module_call_asm(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+			  u64 r10, u64 r11, u64 r12, u64 r13,
+			  struct tdx_module_output *out);
+
+static inline u64 __tdx_module_call(u64 fn, u64 rcx, u64 rdx, u64 r8, u64 r9,
+				    u64 r10, u64 r11, u64 r12, u64 r13,
+				    struct tdx_module_output *out)
+{
+	u64 err, err_masked, retries = 0;
+
+	do {
+		err = __tdx_module_call_asm(fn, rcx, rdx, r8, r9,
+					    r10, r11, r12, r13, out);
+		if (likely(!err) || retries++ > TDX_MODULECALL_RETRY_MAX)
+			break;
+
+		err_masked = err & TDX_MODULECALL_STATUS_MASK;
+	} while (err_masked == TDX_OPERAND_BUSY ||
+		 err_masked == TDX_OPERAND_BUSY_HOST_PRIORITY);
+
+	return err;
+}
 
 void tdx_accept_memory(phys_addr_t start, phys_addr_t end);
 
