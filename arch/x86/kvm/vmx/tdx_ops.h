mateusz-mowka/tboot_linux

KVM: TDX: add the seamcall TDH_MIG_STREAM_CREATE
The TDH_MIG_STREAM_CREATE seamcall creates a migration stream and its
migsc control structure in the TDX module. Migration stream is a TDX
concept and it is analogous to a "channel" in TDX for later TD states
export and import.

The seamcall can be invoked at anytime after the tdcs pages have been
added for the TD, and before a migration session starts.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -396,6 +396,12 @@ static inline u64 tdh_servtd_bind(hpa_t servtd_tdr,
 			    servtd_tdr, slot_idx, type, attr, 0, 0, 0, out);
 }
 
+static inline u64 tdh_mig_stream_create(hpa_t tdr, hpa_t migsc)
+{
+	return kvm_seamcall(TDH_MIG_STREAM_CREATE, migsc, tdr,
+			    0, 0, 0, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -397,6 +397,12 @@ static inline u64 tdh_servtd_bind(hpa_t servtd_tdr,
 			    servtd_tdr, slot_idx, type, attr, 0, 0, 0, out);
 }
 
+static inline u64 tdh_mig_stream_create(hpa_t tdr, hpa_t migsc)
+{
+	return tdx_seamcall(TDH_MIG_STREAM_CREATE, migsc, tdr,
+			    0, 0, 0, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */