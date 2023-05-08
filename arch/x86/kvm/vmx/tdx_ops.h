intel/td-partitioning

KVM: TDX: retry seamcall on recoverable errors
Errors returned by TDX module with the non-recoverable bit (62)
cleared hints that it is likely to be a recoverable error. For
example, TDX_OPERAND_BUSY may be returned when the seamcall fails
to acquire a TDX internal lock. KVM needs to retry the seamcall in
those recoverable error cases.

Add a wrapper of the __seamcall to retry on the recoverable errors.
For the following special cases, retry isn't performed in the wrapper,
the error is instead handed over to the caller to take some action
before retry:
- TDX_VCPU_ASSOCIATED: indicates that the vcpu has been associated to
  another pcpu. Caller needs to disassociate the vcpu before retry.
- TDX_VCPU_NOT_ASSOCIATED: indicates that the vcpu hasn't been
  associated to any pcpu. For tdh_vp_flush, it doesn't need to retry.
- TDX_INTERRUPTED_RESUMABLE: caller usually needs to set a "resume"
  flag before retry the seamcall.

We also don't want to retry the seamcall for unlimited number of times
in case anything logically goes wrong, so set a relative large limit,
TDX_SEAMCALL_RETRY_MAX to capture any unexpected errors.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -19,6 +19,30 @@
 
 #ifdef CONFIG_INTEL_TDX_HOST
 
+static inline u64 kvm_seamcall(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
+			       struct tdx_module_output *out)
+{
+	u64 err, retries = 0;
+
+	do {
+		err = __seamcall(op, rcx, rdx, r8, r9, out);
+		/*
+		 * On success, non-recoverable errors, or recoverable errors
+		 * that don't expect retries, hand it over to the caller.
+		 */
+		if (!err ||
+		    err == TDX_VCPU_ASSOCIATED ||
+		    err == TDX_VCPU_NOT_ASSOCIATED ||
+		    err == TDX_INTERRUPTED_RESUMABLE)
+			return err;
+
+		if (retries++ > TDX_SEAMCALL_RETRY_MAX)
+			break;
+	} while (TDX_SEAMCALL_ERR_RECOVERABLE(err));
+
+	return err;
+}
+
 void pr_tdx_error(u64 op, u64 error_code, const struct tdx_module_output *out);
 
 static inline enum pg_level tdx_sept_level_to_pg_level(int tdx_level)
@@ -130,7 +154,7 @@ static inline u64 tdh_mng_addcx(hpa_t tdr, hpa_t addr)
 	u64 r;
 
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	r = __seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	r = kvm_seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(addr);
 	return r;
@@ -163,7 +187,7 @@ static inline u64 tdh_mem_sept_add(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
 static inline u64 tdh_mem_sept_remove(hpa_t tdr, gpa_t gpa, int level,
 				      struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_SEPT_REMOVE, gpa | level, tdr, 0, 0, out);
+	return kvm_seamcall(TDH_MEM_SEPT_REMOVE, gpa | level, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
@@ -171,7 +195,7 @@ static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
 	u64 r;
 
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	r = __seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	r = kvm_seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(addr);
 	return r;
@@ -181,7 +205,7 @@ static inline u64 tdh_mem_page_relocate(hpa_t tdr, gpa_t gpa, hpa_t hpa,
 					struct tdx_module_output *out)
 {
 	tdx_clflush_page(hpa, PG_LEVEL_4K);
-	return __seamcall(TDH_MEM_PAGE_RELOCATE, gpa, tdr, hpa, 0, out);
+	return kvm_seamcall(TDH_MEM_PAGE_RELOCATE, gpa, tdr, hpa, 0, out);
 }
 
 static inline u64 tdh_mem_page_aug(hpa_t tdr, gpa_t gpa, int level, hpa_t hpa,
@@ -202,12 +226,12 @@ static inline u64 tdh_mem_range_block(hpa_t tdr, gpa_t gpa, int level,
 	/*
 	 * When zapping all secure-ept, kvm->mmu_lock is write locked. No race.
 	 */
-	return __seamcall_sept(TDH_MEM_RANGE_BLOCK, gpa | level, tdr, 0, 0, out);
+	return seamcall_sept(TDH_MEM_RANGE_BLOCK, gpa | level, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mng_key_config(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_CONFIG, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MNG_KEY_CONFIG, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
@@ -215,7 +239,7 @@ static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
 	u64 r;
 
 	tdx_clflush_page(tdr, PG_LEVEL_4K);
-	r = __seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	r = kvm_seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(tdr);
 	return r;
@@ -226,7 +250,7 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 	u64 r;
 
 	tdx_clflush_page(tdvpr, PG_LEVEL_4K);
-	r = __seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	r = kvm_seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(tdvpr);
 	return r;
@@ -234,17 +258,17 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 
 static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
+	return kvm_seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mem_wr(hpa_t tdr, hpa_t addr, u64 val, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_WR, addr, tdr, val, 0, out);
+	return kvm_seamcall(TDH_MEM_WR, addr, tdr, val, 0, out);
 }
 
 static inline u64 tdh_mng_rd(hpa_t tdr, u64 field, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);
+	return kvm_seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);
 }
 
 static inline u64 tdh_mem_page_demote(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
@@ -268,49 +292,49 @@ static inline u64 tdh_mem_page_promote(hpa_t tdr, gpa_t gpa, int level,
 static inline u64 tdh_mr_extend(hpa_t tdr, gpa_t gpa,
 				struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MR_EXTEND, gpa, tdr, 0, 0, out);
+	return kvm_seamcall(TDH_MR_EXTEND, gpa, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mr_finalize(hpa_t tdr)
 {
-	return __seamcall(TDH_MR_FINALIZE, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MR_FINALIZE, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_flush(hpa_t tdvpr)
 {
-	return __seamcall(TDH_VP_FLUSH, tdvpr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_VP_FLUSH, tdvpr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_vpflushdone(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_VPFLUSHDONE, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MNG_VPFLUSHDONE, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_key_freeid(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_FREEID, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MNG_KEY_FREEID, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_init(hpa_t tdr, hpa_t td_params,
 			       struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MNG_INIT, tdr, td_params, 0, 0, out);
+	return kvm_seamcall(TDH_MNG_INIT, tdr, td_params, 0, 0, out);
 }
 
 static inline u64 tdh_vp_init(hpa_t tdvpr, u64 rcx)
 {
-	return __seamcall(TDH_VP_INIT, tdvpr, rcx, 0, 0, NULL);
+	return kvm_seamcall(TDH_VP_INIT, tdvpr, rcx, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_rd(hpa_t tdvpr, u64 field,
 			    struct tdx_module_output *out)
 {
-	return __seamcall(TDH_VP_RD, tdvpr, field, 0, 0, out);
+	return kvm_seamcall(TDH_VP_RD, tdvpr, field, 0, 0, out);
 }
 
 static inline u64 tdh_mng_key_reclaimid(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_RECLAIMID, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MNG_KEY_RECLAIMID, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_phymem_page_reclaim(hpa_t page,
@@ -318,7 +342,7 @@ static inline u64 tdh_phymem_page_reclaim(hpa_t page,
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX;
-	return __seamcall(TDH_PHYMEM_PAGE_RECLAIM, page, 0, 0, 0, out);
+	return kvm_seamcall(TDH_PHYMEM_PAGE_RECLAIM, page, 0, 0, 0, out);
 }
 
 static inline u64 tdh_mem_page_remove(hpa_t tdr, gpa_t gpa, int level,
@@ -329,14 +353,14 @@ static inline u64 tdh_mem_page_remove(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_sys_lp_shutdown(void)
 {
-	return __seamcall(TDH_SYS_LP_SHUTDOWN, 0, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_SYS_LP_SHUTDOWN, 0, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mem_track(hpa_t tdr)
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_TD_EPOCH;
-	return __seamcall(TDH_MEM_TRACK, tdr, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_MEM_TRACK, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mem_range_unblock(hpa_t tdr, gpa_t gpa, int level,
@@ -347,20 +371,21 @@ static inline u64 tdh_mem_range_unblock(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_phymem_cache_wb(bool resume)
 {
-	return __seamcall(TDH_PHYMEM_CACHE_WB, resume ? 1 : 0, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_PHYMEM_CACHE_WB,
+			    resume ? 1 : 0, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_phymem_page_wbinvd(hpa_t page)
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX;
-	return __seamcall(TDH_PHYMEM_PAGE_WBINVD, page, 0, 0, 0, NULL);
+	return kvm_seamcall(TDH_PHYMEM_PAGE_WBINVD, page, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_wr(hpa_t tdvpr, u64 field, u64 val, u64 mask,
 			    struct tdx_module_output *out)
 {
-	return __seamcall(TDH_VP_WR, tdvpr, field, val, mask, out);
+	return kvm_seamcall(TDH_VP_WR, tdvpr, field, val, mask, out);
 }
 #endif /* CONFIG_INTEL_TDX_HOST */
 

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -19,6 +19,30 @@
 
 #ifdef CONFIG_INTEL_TDX_HOST
 
+static inline u64 tdx_seamcall(u64 op, u64 rcx, u64 rdx, u64 r8, u64 r9,
+			       struct tdx_module_output *out)
+{
+	u64 err, retries = 0;
+
+	do {
+		err = __seamcall(op, rcx, rdx, r8, r9, out);
+		/*
+		 * On success, non-recoverable errors, or recoverable errors
+		 * that don't expect retries, hand it over to the caller.
+		 */
+		if (!err ||
+		    err == TDX_VCPU_ASSOCIATED ||
+		    err == TDX_VCPU_NOT_ASSOCIATED ||
+		    err == TDX_INTERRUPTED_RESUMABLE)
+			return err;
+
+		if (retries++ > TDX_SEAMCALL_RETRY_MAX)
+			break;
+	} while (TDX_SEAMCALL_ERR_RECOVERABLE(err));
+
+	return err;
+}
+
 void pr_tdx_error(u64 op, u64 error_code, const struct tdx_module_output *out);
 
 static inline enum pg_level tdx_sept_level_to_pg_level(int tdx_level)
@@ -130,7 +154,7 @@ static inline u64 tdh_mng_addcx(hpa_t tdr, hpa_t addr)
 	u64 r;
 
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	r = __seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_MNG_ADDCX, addr, tdr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(addr);
 	return r;
@@ -163,7 +187,7 @@ static inline u64 tdh_mem_sept_add(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
 static inline u64 tdh_mem_sept_remove(hpa_t tdr, gpa_t gpa, int level,
 				      struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_SEPT_REMOVE, gpa | level, tdr, 0, 0, out);
+	return tdx_seamcall(TDH_MEM_SEPT_REMOVE, gpa | level, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
@@ -171,7 +195,7 @@ static inline u64 tdh_vp_addcx(hpa_t tdvpr, hpa_t addr)
 	u64 r;
 
 	tdx_clflush_page(addr, PG_LEVEL_4K);
-	r = __seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_VP_ADDCX, addr, tdvpr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(addr);
 	return r;
@@ -181,7 +205,7 @@ static inline u64 tdh_mem_page_relocate(hpa_t tdr, gpa_t gpa, hpa_t hpa,
 					struct tdx_module_output *out)
 {
 	tdx_clflush_page(hpa, PG_LEVEL_4K);
-	return __seamcall(TDH_MEM_PAGE_RELOCATE, gpa, tdr, hpa, 0, out);
+	return tdx_seamcall(TDH_MEM_PAGE_RELOCATE, gpa, tdr, hpa, 0, out);
 }
 
 static inline u64 tdh_mem_page_aug(hpa_t tdr, gpa_t gpa, int level, hpa_t hpa,
@@ -202,12 +226,12 @@ static inline u64 tdh_mem_range_block(hpa_t tdr, gpa_t gpa, int level,
 	/*
 	 * When zapping all secure-ept, kvm->mmu_lock is write locked. No race.
 	 */
-	return __seamcall_sept(TDH_MEM_RANGE_BLOCK, gpa | level, tdr, 0, 0, out);
+	return seamcall_sept(TDH_MEM_RANGE_BLOCK, gpa | level, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mng_key_config(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_CONFIG, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MNG_KEY_CONFIG, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
@@ -215,7 +239,7 @@ static inline u64 tdh_mng_create(hpa_t tdr, int hkid)
 	u64 r;
 
 	tdx_clflush_page(tdr, PG_LEVEL_4K);
-	r = __seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
+	r = tdx_seamcall(TDH_MNG_CREATE, tdr, hkid, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(tdr);
 	return r;
@@ -226,7 +250,7 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 	u64 r;
 
 	tdx_clflush_page(tdvpr, PG_LEVEL_4K);
-	r = __seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
+	r = tdx_seamcall(TDH_VP_CREATE, tdvpr, tdr, 0, 0, NULL);
 	if (!r)
 		tdx_set_page_np(tdvpr);
 	return r;
@@ -234,17 +258,17 @@ static inline u64 tdh_vp_create(hpa_t tdr, hpa_t tdvpr)
 
 static inline u64 tdh_mem_rd(hpa_t tdr, gpa_t addr, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
+	return tdx_seamcall(TDH_MEM_RD, addr, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mem_wr(hpa_t tdr, hpa_t addr, u64 val, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MEM_WR, addr, tdr, val, 0, out);
+	return tdx_seamcall(TDH_MEM_WR, addr, tdr, val, 0, out);
 }
 
 static inline u64 tdh_mng_rd(hpa_t tdr, u64 field, struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);
+	return tdx_seamcall(TDH_MNG_RD, tdr, field, 0, 0, out);
 }
 
 static inline u64 tdh_mem_page_demote(hpa_t tdr, gpa_t gpa, int level, hpa_t page,
@@ -268,49 +292,49 @@ static inline u64 tdh_mem_page_promote(hpa_t tdr, gpa_t gpa, int level,
 static inline u64 tdh_mr_extend(hpa_t tdr, gpa_t gpa,
 				struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MR_EXTEND, gpa, tdr, 0, 0, out);
+	return tdx_seamcall(TDH_MR_EXTEND, gpa, tdr, 0, 0, out);
 }
 
 static inline u64 tdh_mr_finalize(hpa_t tdr)
 {
-	return __seamcall(TDH_MR_FINALIZE, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MR_FINALIZE, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_flush(hpa_t tdvpr)
 {
-	return __seamcall(TDH_VP_FLUSH, tdvpr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_VP_FLUSH, tdvpr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_vpflushdone(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_VPFLUSHDONE, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MNG_VPFLUSHDONE, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_key_freeid(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_FREEID, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MNG_KEY_FREEID, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mng_init(hpa_t tdr, hpa_t td_params,
 			       struct tdx_module_output *out)
 {
-	return __seamcall(TDH_MNG_INIT, tdr, td_params, 0, 0, out);
+	return tdx_seamcall(TDH_MNG_INIT, tdr, td_params, 0, 0, out);
 }
 
 static inline u64 tdh_vp_init(hpa_t tdvpr, u64 rcx)
 {
-	return __seamcall(TDH_VP_INIT, tdvpr, rcx, 0, 0, NULL);
+	return tdx_seamcall(TDH_VP_INIT, tdvpr, rcx, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_rd(hpa_t tdvpr, u64 field,
 			    struct tdx_module_output *out)
 {
-	return __seamcall(TDH_VP_RD, tdvpr, field, 0, 0, out);
+	return tdx_seamcall(TDH_VP_RD, tdvpr, field, 0, 0, out);
 }
 
 static inline u64 tdh_mng_key_reclaimid(hpa_t tdr)
 {
-	return __seamcall(TDH_MNG_KEY_RECLAIMID, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MNG_KEY_RECLAIMID, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_phymem_page_reclaim(hpa_t page,
@@ -318,7 +342,7 @@ static inline u64 tdh_phymem_page_reclaim(hpa_t page,
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX;
-	return __seamcall(TDH_PHYMEM_PAGE_RECLAIM, page, 0, 0, 0, out);
+	return tdx_seamcall(TDH_PHYMEM_PAGE_RECLAIM, page, 0, 0, 0, out);
 }
 
 static inline u64 tdh_mem_page_remove(hpa_t tdr, gpa_t gpa, int level,
@@ -329,14 +353,14 @@ static inline u64 tdh_mem_page_remove(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_sys_lp_shutdown(void)
 {
-	return __seamcall(TDH_SYS_LP_SHUTDOWN, 0, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_SYS_LP_SHUTDOWN, 0, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mem_track(hpa_t tdr)
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_TD_EPOCH;
-	return __seamcall(TDH_MEM_TRACK, tdr, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_MEM_TRACK, tdr, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_mem_range_unblock(hpa_t tdr, gpa_t gpa, int level,
@@ -347,20 +371,21 @@ static inline u64 tdh_mem_range_unblock(hpa_t tdr, gpa_t gpa, int level,
 
 static inline u64 tdh_phymem_cache_wb(bool resume)
 {
-	return __seamcall(TDH_PHYMEM_CACHE_WB, resume ? 1 : 0, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_PHYMEM_CACHE_WB,
+			    resume ? 1 : 0, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_phymem_page_wbinvd(hpa_t page)
 {
 	if (tdx_op_is_busy())
 		return TDX_OPERAND_BUSY | TDX_OPERAND_ID_RCX;
-	return __seamcall(TDH_PHYMEM_PAGE_WBINVD, page, 0, 0, 0, NULL);
+	return tdx_seamcall(TDH_PHYMEM_PAGE_WBINVD, page, 0, 0, 0, NULL);
 }
 
 static inline u64 tdh_vp_wr(hpa_t tdvpr, u64 field, u64 val, u64 mask,
 			    struct tdx_module_output *out)
 {
-	return __seamcall(TDH_VP_WR, tdvpr, field, val, mask, out);
+	return tdx_seamcall(TDH_VP_WR, tdvpr, field, val, mask, out);
 }
 #endif /* CONFIG_INTEL_TDX_HOST */
 