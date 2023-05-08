intel/td-partitioning

KVM: TDX: add seamcall TDH_SERVTD_PREBIND
The TDH_SERVTD_PREBIND seamcall pre-binds a user TD to a service TD. A
service TD is a special TD launched in the same way as launching a regular
user TD, and its job is to provide services to the user TDs  that it has
been bound to, e.g. assist in the process of live migrating the user TD.

Service TD is part of the user TD's TCB, so it needs to be included in
the user TD's attestion process. With pre-binding, a hash value of the
service TD's TDINFO_STRUCT is provided.

The pre-binding is performed during the process of launching a user TD
before the TDH_MR_FINALIZE seamcall is made. At the time of pre-binding,
the service TD does NOT need to be running. The below parameters are
provided by the caller:
- target_tdr: the tdr page of the target user TD
- hash_addr: physical address of the page that stores a SHA384 hash value
  of the service TD's TDINFO_STRUCT.
- slot_idx: index into the user TD's service TD binding table, which is
  internally maintained by the TDX module.
- attr: service TD binding attributes, provided by user following Table
  4.53 defined in the TDX ABI spec.
- type: service TD type, defined in Table 4.52, TDX ABI spec. The type
  is usally passed from userspace, so "enum kvm_tdx_servtd_type" is
  exposed in uapi.

The main purpose of pre-binding is to facilitate the build of a transient
service TD. For example, a user TD may need services from a service TD
later in its lifecycle (e.g. services to assist live migration when needed
later). In this case, the service TD doesn't have to be launched before
launching the user TD, and it can be launched on demand when the service
is needed, and after the service is done, the service TD can be
terminated.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -374,6 +374,17 @@ static inline u64 tdh_vp_wr(hpa_t tdvpr, u64 field, u64 val, u64 mask,
 	return kvm_seamcall(TDH_VP_WR,
 			    tdvpr, field, val, mask, 0, 0, 0, 0, out);
 }
+
+static inline u64 tdh_servtd_prebind(hpa_t target_tdr,
+				     hpa_t hash_addr,
+				     u64 slot_idx,
+				     u64 attr,
+				     enum kvm_tdx_servtd_type type)
+{
+	return kvm_seamcall(TDH_SERVTD_PREBIND, target_tdr,
+			    hash_addr, slot_idx, type, attr, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -375,6 +375,17 @@ static inline u64 tdh_vp_wr(hpa_t tdvpr, u64 field, u64 val, u64 mask,
 	return tdx_seamcall(TDH_VP_WR,
 			    tdvpr, field, val, mask, 0, 0, 0, 0, out);
 }
+
+static inline u64 tdh_servtd_prebind(hpa_t target_tdr,
+				     hpa_t hash_addr,
+				     u64 slot_idx,
+				     u64 attr,
+				     enum kvm_tdx_servtd_type type)
+{
+	return tdx_seamcall(TDH_SERVTD_PREBIND, target_tdr,
+			    hash_addr, slot_idx, type, attr, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */