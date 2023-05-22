mateusz-mowka/tboot_linux

KVM: TDX: add seamcall TDX_EXPORT_STATE_IMMUTABLE
The TDX_EXPORT_STATE_IMMUTABLE seamcall starts a new migration session
on the source side and exports the TD-scope immutable state. The
exported data includes 2 parts: 1) MBMD, which is the metadata of the
exported state data; and 2) the immutable state data.

The mbmd_info field provides a buffer to load MBMD, and its format is
defined in the TDX module API, as below:
Bits 51:0  - Host physical address of the buffer, including HKID bits
Bits 63:52 - Size of the buffer
The page_list_info field provides a list of 4KB pages used as buffers to
store the TD-scope immutable state data.
The mig_stream_info field provides the info about the migration stream
(should have been created through TDH_MIG_STREAM_CREATE ) from the TDX
module to load the state. Its format:
Bits 15:0  - migration stream index (must be 0)
Bits 62:16 - reserved (must be 0)
Bits 63    - Indicates if this is resumption of a previously interrupted
             operation (being 1) or a new invocation (being 0).

The seamcall is interruptible, and the caller is expected to invoke it
in a loop when the returning status is TDX_INTERRUPTED_RESUMABLE, and
the above resumption flag (bit 63) of mig_stream_info needs to be set.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -418,6 +418,16 @@ static inline u64 tdh_export_unblockw(hpa_t tdr,
 			    0, 0, 0, 0, 0, 0, out);
 }
 
+static inline u64 tdh_export_state_immutable(hpa_t tdr,
+					     u64 mbmd_info,
+					     u64 page_list_info,
+					     u64 mig_stream_info,
+					     struct tdx_module_output *out)
+{
+	return kvm_seamcall(TDH_EXPORT_STATE_IMMUTABLE, tdr, 0, mbmd_info,
+			    page_list_info, mig_stream_info, 0, 0, 0, out);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -419,6 +419,16 @@ static inline u64 tdh_export_unblockw(hpa_t tdr,
 			    0, 0, 0, 0, 0, 0, out);
 }
 
+static inline u64 tdh_export_state_immutable(hpa_t tdr,
+					     u64 mbmd_info,
+					     u64 page_list_info,
+					     u64 mig_stream_info,
+					     struct tdx_module_output *out)
+{
+	return tdx_seamcall(TDH_EXPORT_STATE_IMMUTABLE, tdr, 0, mbmd_info,
+			    page_list_info, mig_stream_info, 0, 0, 0, out);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */