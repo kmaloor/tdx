mateusz-mowka/tboot_linux

KVM: TDX: add seamcall TDH_IMPORT_TRACK
The TDH_IMPORT_TRACK seamcall imports an epoch data from the source.
According to the epoch type, the TDX module will either starts a new
in-order epoch phase or enter into the out-of-order phase.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -475,6 +475,14 @@ static inline u64 tdh_export_track(hpa_t tdr,
 			    mig_stream_info, 0, 0, 0, NULL);
 }
 
+static inline u64 tdh_import_track(hpa_t tdr,
+				    u64 mbmd_info,
+				    u64 mig_stream_info)
+{
+	return kvm_seamcall(TDH_IMPORT_TRACK, tdr, 0, mbmd_info, 0,
+			    mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -476,6 +476,14 @@ static inline u64 tdh_export_track(hpa_t tdr,
 			    mig_stream_info, 0, 0, 0, NULL);
 }
 
+static inline u64 tdh_import_track(hpa_t tdr,
+				    u64 mbmd_info,
+				    u64 mig_stream_info)
+{
+	return tdx_seamcall(TDH_IMPORT_TRACK, tdr, 0, mbmd_info, 0,
+			    mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */