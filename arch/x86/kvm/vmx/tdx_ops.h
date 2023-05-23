mateusz-mowka/tboot_linux

KVM: TDX: add seamcall TDH_EXPORT_TRACK
The TDH_EXPORT_TRACK seamcall generates an epoch token used during memory
export and import. There are two types of epoch token:
1) in-order phase epoch: required to be exported at the beginning of each
  iteration of memory export. The restriction for in-order phase is that
  in each iteration, the same guest-physical priavte page can only be
  exported once.
2) out-of-order starting epoch: the epoch indicates the end of in-order
  phase, meaning that the above restriction is gone.

The type of epoch to generate is configured via bit 63 of the
mig_stream_info field, and the epoch data is generated into the buffer
offered by mbmd_info.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -467,6 +467,14 @@ static inline u64 tdh_import_mem(hpa_t tdr,
 			    mac_list1_info, td_page_list_info, out);
 }
 
+static inline u64 tdh_export_track(hpa_t tdr,
+				   u64 mbmd_info,
+				   u64 mig_stream_info)
+{
+	return kvm_seamcall(TDH_EXPORT_TRACK, tdr, 0, mbmd_info, 0,
+			    mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

  in each iteration, the same guest-physical private page can only be

arch/x86/kvm/vmx/tdx_ops.h
@@ -468,6 +468,14 @@ static inline u64 tdh_import_mem(hpa_t tdr,
 			    mac_list1_info, td_page_list_info, out);
 }
 
+static inline u64 tdh_export_track(hpa_t tdr,
+				   u64 mbmd_info,
+				   u64 mig_stream_info)
+{
+	return tdx_seamcall(TDH_EXPORT_TRACK, tdr, 0, mbmd_info, 0,
+			    mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */