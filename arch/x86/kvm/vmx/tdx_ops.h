mateusz-mowka/tboot_linux

KVM: TDX: add seamcall TDH_EXPORT_ABORT
The TDH_EXPORT_ABORT aborts an export session. The MBMD buffer offered via
mbmd_info stores an token generated by TDH_IMPORT_ABORT on the destination
if it is aborted during the out-of-order phase.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -533,6 +533,14 @@ static inline u64 tdh_import_state_vp(hpa_t tdvpr,
 			    buf_list_info, mig_stream_info, 0, 0, 0, out);
 }
 
+static inline u64 tdh_export_abort(hpa_t tdr,
+				   u64 mbmd_info,
+				   u64 mig_stream_info)
+{
+	return kvm_seamcall(TDH_EXPORT_ABORT, tdr, 0, mbmd_info,
+			    0, mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -534,6 +534,14 @@ static inline u64 tdh_import_state_vp(hpa_t tdvpr,
 			    buf_list_info, mig_stream_info, 0, 0, 0, out);
 }
 
+static inline u64 tdh_export_abort(hpa_t tdr,
+				   u64 mbmd_info,
+				   u64 mig_stream_info)
+{
+	return tdx_seamcall(TDH_EXPORT_ABORT, tdr, 0, mbmd_info,
+			    0, mig_stream_info, 0, 0, 0, NULL);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */