mateusz-mowka/tboot_linux

KVM: TDX: add seamcall TDH_IMPORT_STATE_TD
The TDH_IMPORT_STATE_TD seamcall imports the TD-scope states generated by
TDH_EXPORT_STATE_TD on the source. The state data is stored in a list of
shared pages offered via buf_list_info.

Signed-off-by: Wei Wang <wei.w.wang@intel.com>

arch/x86/kvm/vmx/tdx_ops.h
@@ -503,6 +503,16 @@ static inline u64 tdh_export_state_td(hpa_t tdr,
 			    buf_list_info, mig_stream_info, 0, 0, 0, out);
 }
 
+static inline u64 tdh_import_state_td(hpa_t tdr,
+				      u64 mbmd_info,
+				      u64 buf_list_info,
+				      u64 mig_stream_info,
+				      struct tdx_module_output *out)
+{
+	return kvm_seamcall(TDH_IMPORT_STATE_TD, tdr, 0, mbmd_info,
+			    buf_list_info, mig_stream_info, 0, 0, 0, out);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */

kmaloor/tdx

arch/x86/kvm/vmx/tdx_ops.h
@@ -504,6 +504,16 @@ static inline u64 tdh_export_state_td(hpa_t tdr,
 			    buf_list_info, mig_stream_info, 0, 0, 0, out);
 }
 
+static inline u64 tdh_import_state_td(hpa_t tdr,
+				      u64 mbmd_info,
+				      u64 buf_list_info,
+				      u64 mig_stream_info,
+				      struct tdx_module_output *out)
+{
+	return tdx_seamcall(TDH_IMPORT_STATE_TD, tdr, 0, mbmd_info,
+			    buf_list_info, mig_stream_info, 0, 0, 0, out);
+}
+
 #endif /* CONFIG_INTEL_TDX_HOST */
 
 #endif /* __KVM_X86_TDX_OPS_H */